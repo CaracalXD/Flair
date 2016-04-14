28395946d9b3ed23a081b416cda2f5bda151851a
caracal <rcruelljr@gmail.com>
2016-04-13 20:28:47 -0700

    Add nature_call

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/woo')
+def nature_call():
+    print "woooooooooo"
+    
 if __name__ == '__main__':
     app.run()
